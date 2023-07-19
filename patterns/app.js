dc3359fbb9fa3dc0f6c35fa054f93c9e8bf1a7d7
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-07-18 22:49:12 -0400

    object created for linkedlist

--- patterns/app.js
+++ patterns/app.js
@@ -1,12 +1,26 @@
 // creates node for linked-list
 class Node {
 
-    constructor(element, ref) {
+    constructor(element) {
         this.element = element
-        this.ref = ref
+        this.next = null
     }
 }
 
 class LinkedList {
 
+    constructor(head = null) {
+        this.head = head
+    }
 }
+
+var first = new Node(1)
+var second = new Node(2)
+var third = new Node(3)
+
+var list = new LinkedList(first)
+
+first.next = second
+second.next = third
+
+console.log(list)

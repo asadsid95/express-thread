99871c62f6fa31bd8d1738af29a4574610d0dce7
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-07-20 03:58:18 -0400

    minor changes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
-
 const app = express()
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
 
 app.get('/api/v1/tasks', (req, res) => {
     res.send('get Tasks')
 })
+
+app.use('/api/v1/tasks', tasks)
+
 app.put('/api/v1/tasks', (req, res) => {
     res.send('At /api/v1/tasks')
 })

694a2531aa2cfd72e48c67caeed009a856e98760
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-03 16:11:42 -0400

    callback hell

--- patterns/callback_hell.js
+++ patterns/callback_hell.js
@@ -3,17 +3,42 @@ function asyncFunction(data, callback) {
 
     console.log('timer about to run for 2000 ms')
     setTimeout(() => {
-        console.log(`asyncFunction with data ${data} runs after 2 seconds.`)
-        callback()
 
+        var createRandomError = 3 > 2 ? new Error('Error due to randomness') : null
+
+        if (createRandomError) {
+            console.error(createRandomError.message)
+            callback(createRandomError)
+        } else {
+            console.log(`asyncFunction with data ${data} runs after 2 seconds.`)
+            callback()
+        }
     }, 2000)
 
 }
 
-function print() {
+function print(error, somevalue) {
+    if (error) { throw new Error('new Error while printing') }
     console.log('~~~print~~~')
 }
 
-asyncFunction('[hello]', () =>
-    asyncFunction("[hello]", print)
+asyncFunction('[hello]', (errA) => {
+    if (errA) {
+        console.log(errA.message)
+    } else {
+        asyncFunction("[there]", () => asyncFunction('[, this is data]', print))
+    }
+}
 )
+
+// asyncfunction sends data which after 2 seconds gets used and then the callback fires. Callback happens to be the same function with different data. This new data gets used after 2 seconds and then the callback fires which is print()
+
+// expected output without error handling: ///
+
+// timer about to run for 2000 ms
+// asyncFunction with data [hello] runs after 2 seconds.
+
+// timer about to run for 2000 ms
+// asyncFunction with data [there] runs after 2 seconds.
+
+// ~~~print~~~

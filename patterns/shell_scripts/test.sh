1972de4cb4f703d4b2c4bf5894404c0de4ac9876
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-03 17:40:38 -0400

    tested deletion of file but accessing its content in on-going process

---
+++ patterns/shell_scripts/test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo 'This is a test file that will be used by a process but deleted meanwhile'
+
+
+# $ tail -f test.txt &
+# $ rm text.txt
+# $ ls -l 
+# $ cat test.txt
+# ps
+# $ kill %1

3f150c0421f1e0440d6a7fac99bd6c16508887b2
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-23 00:36:12 -0400

    another solution for climbstairs

--- Leetcode/Climbing-Stairs.js
+++ Leetcode/Climbing-Stairs.js
@@ -4,16 +4,32 @@
  */
 var climbStairs = function(n) {
     
-    // 0th index indicates ground zero
-    var array = new Array(n+1)
+    // // 0th index indicates ground zero
+    // var array = new Array(n+1)
 
-    if (n==1) return n
+    // // stairs of 1 step only has 1 way
+    // // stairs of 2 steps has 2 ways
+    // if (n==1) return n
 
-    array[1] = 1;
-    array[2] = 2;
+    // // initializing the array 
+    // array[1] = 1;
+    // array[2] = 2;
 
-    for(let i =3; i <= n; i++) {
+    // // to get to the top, the last traversal can happen either from the last step or second last step
+    // // so getting the numbers of way to reach those two (last and second-last step) and adding them sums the possibilities
+    // for(let i =3; i <= n; i++) {
+    //     array[i] = array[i-1] + array[i-2]
+    // }
+
+    // return array[n]
+
+    var array = [0,1,2]
+
+    // how is the array expanding
+    for(let i=3;i<=n;i++){
         array[i] = array[i-1] + array[i-2]
+
+        console.log(array)
     }
 
     return array[n]
@@ -21,3 +37,8 @@ var climbStairs = function(n) {
 };
 
 console.log(climbStairs(6))
+
+// for time complexity,
+// loop iterates over an array of n-3
+// constant of 3 is removed so O(n) 
+

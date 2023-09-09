483a3e2480acfe0118ecb7fa3dbc50987e836c59
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-08 21:39:03 -0400

    solution for longest increasing subsequence in progress; learning binary search

---
+++ Leetcode/300-Longest-Increasing-Subsequence.js
@@ -0,0 +1,23 @@
+
+
+nums = [1,2,5,6,7]
+
+let left = 0
+let right = (nums.length - 1 )
+
+let foo = 3
+// console.log(mid )
+while (left < right) {
+    
+    let mid = Math.floor((left + right)/2)
+    
+    if(nums[mid] < 3) {
+        let 
+
+    } else {
+
+    }
+
+    break
+
+}

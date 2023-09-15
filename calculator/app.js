1c7750d7c0ecf45371b938043a61751f32d9cfdd
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-15 17:57:24 -0400

    incomplete logic for handling operation

--- calculator/app.js
+++ calculator/app.js
@@ -5,3 +5,52 @@
 // ask user to enter second digits
 
 // ask user to select = sign
+
+// select display element
+function clearDisplay() {
+
+    // change display element's value to 0
+    digits = ''
+    display.value = 0
+}
+
+function calculateResult() {
+
+}
+
+
+let display = document.getElementById('display')
+let first_digits = ''
+let operation = ''
+let second_digits = ''
+
+// provide value to display element
+// function appendToDisplay(value) {
+
+//     {/* 
+//         check if value is decimal 
+//             - if digits don't have a decimal 
+//               add decimal to digits 
+//         else, add non-decimal value to digits
+
+//     */} 
+//     if (value === '.'){
+//         if(first_digits.indexOf('.') == -1 ){
+//             first_digits += value
+
+//         } 
+//         // else {
+//         //     // if decimal does exist, it should do nothing
+//         // }
+//     } else {
+//         first_digits += value
+//         display.value = first_digits
+//     }
+
+//     // edge case: preventing leading zeros
+
+// }
+
+
+
+

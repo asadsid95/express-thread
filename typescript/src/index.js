db0114d74706fa78608d907e969c0c4d664208fd
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-10-04 07:35:42 -0400

    functions

--- typescript/src/index.js
+++ typescript/src/index.js
@@ -2,41 +2,23 @@
 // annotation not required for 
 // let sales: number = 202_202_202
 // let course: string = 'TypeScript course'
-
 // let is_published: boolean = true
-
 // gives compilation error
 // let number: number[] = [1, 2, '3', true]
-
 // let number: number[] = []
-
 // number.forEach(n => n.)
-
-
 // tuple - fixed length of array where each element has a specific type; create it by specifying types of each element of the array
 // specifying each element type; any extra element will give compilation error
-let user: [number, string] = [1, 'ASAD']
-// tuples are good for array length of 2 i.e. k/v pair
-
-
-// enums - list of related constants
-// adding const to enum generates more concise of JS
-const enum Size { Small = 's', Medium = 'm', Large = 'lg' }
-let mySize: Size = Size.Medium
-console.log(mySize)
-
-
+var user = [1, 'ASAD'];
+var mySize = "m" /* Size.Medium */;
+console.log(mySize);
 // function
 // setting in tsconfig to prevent unused parameters; another setting to prevent implicit returns; another setting to prevent unused local 
-
-function calculateTax(income: number, taxYear?): number {
-
-    let taxCredit = 2_000
-
-    if (income < 30_000) {
-        return income * 1.1
+function calculateTax(income, taxYear) {
+    var taxCredit = 2000;
+    if (income < 30000) {
+        return income * 1.1;
     }
-    return income * 1.3
+    return income * 1.3;
 }
-
-calculateTax(4000)
+calculateTax(4000);
